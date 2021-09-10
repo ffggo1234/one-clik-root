e101814a47a824add67acbd1af14de6842b5a54b
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-09-10 12:02:46 +0800

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo lsattr /etc/passwd /etc/shadow
+sudo chattr -i /etc/passwd /etc/shadow
+sudo chattr -a /etc/passwd /etc/shadow
+sudo lsattr /etc/passwd /etc/shadow
+
+read -p "自定义root密码:" mima
+echo root:$mima | sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+sudo reboot
