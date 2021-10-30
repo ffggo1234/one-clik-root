595e6e66784babeb6c77cf6f83d4b8e0efd55898
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-10-30 13:02:51 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
 sudo lsattr /etc/passwd /etc/shadow
 sudo chattr -i /etc/passwd /etc/shadow
 sudo chattr -a /etc/passwd /etc/shadow
@@ -10,4 +14,5 @@ echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
-sudo reboot
+green "VPS当前用户名：root"
+green "vps当前密码：$mima"
