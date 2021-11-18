25c283859ad96f0de1847b7e363a2cbf2e32208f
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-11-19 05:54:58 +0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -497,13 +497,13 @@ so(){
 if [[ ! $(type -P warp-cli) ]]; then
 red "SOCKS5的WARP未安装，无法启动或关闭，建议重新安装WARP(+)"
 fi
-if [[ $(type -P wg-cli) ]] && [[ $(warp-cli --accept-tos status 2>/dev/null) =~ Connected ]]; then
+if [[ $(type -P wg-cli) ]] && [[ $(warp-cli --accept-tos status 2>/dev/null | grep Connected) ]]; then
 yellow "当前WARP(+)为--已开启状态，现执行:临时关闭……"
 sleep 1s
 warp-cli --accept-tos disable-always-on >/dev/null 2>&1
 green "临时关闭WARP(+)成功"
 fi
-if [[ $(type -P wg-cli) ]] && [[ $(warp-cli --accept-tos status 2>/dev/null) =~ Disconnected ]]; then
+if [[ $(type -P wg-cli) ]] && [[ $(warp-cli --accept-tos status 2>/dev/null | grep Disconnected) ]]; then
 yellow "当前WARP(+)为--临时关闭状态，现执行:恢复开启……"
 sleep 1s
 warp-cli --accept-tos enable-always-on >/dev/null 2>&1
