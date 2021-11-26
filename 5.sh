603f753755ee8616484b674adccb526ed54a8567
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-11-26 16:38:57 +0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+export LANG=en_US.UTF-8
 
 red(){ echo -e "\033[31m\033[01m$1\033[0m";}
 green(){ echo -e "\033[32m\033[01m$1\033[0m";}
