b4a80308d7b8d23d8f3a24bea5509f9a648a9ccc
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-11-27 20:37:59 +0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -59,7 +59,7 @@ g4=$(eval echo \$$gj4)
 WARPIPv4=$(curl -s4m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
 case ${WARPIPv4} in 
 plus) 
-WARPIPv4Status=$(white "IPV4 WARP(+)状态：\c" ; rred "WARP+PLUS运行中" ; white " IPV4 当前地址：\c" ; rred "$v44" ; white " IPV4 所在区域：\c" ; rred "$g4" ; white " IPV4 IP服务商：\c" ; rred "$isp4" ; white " WGCF+账号设备名称：\c" ; rred "$(grep -s 'Device name' /etc/wireguard/WGCF+.log | awk '{ print $NF }')" ; white "WGCF+账号剩余流量：\c" ; rred "$(grep -s Quota /etc/wireguard/WGCF+.log | awk '{ print $(NF-1), $NF }')") 
+WARPIPv4Status=$(white "IPV4 WARP(+)状态：\c" ; rred "WARP+PLUS运行中" ; white " IPV4 当前地址：\c" ; rred "$v44" ; white " IPV4 所在区域：\c" ; rred "$g4" ; white " IPV4 IP服务商：\c" ; rred "$isp4" ; white " WGCF+账号设备名称：\c" ; rred "$(grep -s 'Device name' /etc/wireguard/wgcf+p.log | awk '{ print $NF }')" ; white "WGCF+账号剩余流量：\c" ; rred "$(grep -s Quota /etc/wireguard/wgcf+p.log | awk '{ print $(NF-1), $NF }')") 
 ;;  
 on) 
 WARPIPv4Status=$(white "IPV4 WARP状态：\c" ; green "WARP运行中" ; white " IPV4 当前地址：\c" ; green "$v44" ; white " IPV4 所在区域：\c" ; green "$g4" ; white " IPV4 IP服务商：\c" ; green "$isp4")
@@ -76,7 +76,7 @@ g6=$(eval echo \$$gj6)
 WARPIPv6Status=$(curl -s6m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
 case ${WARPIPv6Status} in 
 plus) 
-WARPIPv6Status=$(white "IPV6 WARP(+)状态：\c" ; rred "WARP+PLUS运行中" ; white " IPV6 当前地址：\c" ; rred "$v66" ; white " IPV6 所在区域：\c" ; rred "$g6" ; white " IPV6 IP服务商：\c" ; rred "$isp6" ; white " WGCF+账号设备名称：\c" ; rred "$(grep -s 'Device name' /etc/wireguard/WGCF+.log | awk '{ print $NF }')" ; white "WGCF+账号剩余流量：\c" ; rred "$(grep -s Quota /etc/wireguard/WGCF+.log | awk '{ print $(NF-1), $NF }')") 
+WARPIPv6Status=$(white "IPV6 WARP(+)状态：\c" ; rred "WARP+PLUS运行中" ; white " IPV6 当前地址：\c" ; rred "$v66" ; white " IPV6 所在区域：\c" ; rred "$g6" ; white " IPV6 IP服务商：\c" ; rred "$isp6" ; white " WGCF+账号设备名称：\c" ; rred "$(grep -s 'Device name' /etc/wireguard/wgcf+p.log | awk '{ print $NF }')" ; white "WGCF+账号剩余流量：\c" ; rred "$(grep -s Quota /etc/wireguard/wgcf+p.log | awk '{ print $(NF-1), $NF }')") 
 ;;  
 on) 
 WARPIPv6Status=$(white "IPV6 WARP状态：\c" ; green "WARP运行中" ; white " IPV6 当前地址：\c" ; green "$v66" ; white " IPV6 所在区域：\c" ; green "$g6" ; white " IPV6 IP服务商：\c" ; green "$isp6")
