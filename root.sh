a88f6c68e422ef0c4a6395a1d5a9484122dbd65b
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-12-29 05:49:20 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,15 +4,15 @@ green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
 
-sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
-sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
-sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
-sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
+chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
+lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 
 read -p "自定义root密码:" mima
-echo root:$mima | sudo chpasswd root
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo service sshd restart
+echo root:$mima | chpasswd root
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+service sshd restart
 green "VPS当前用户名：root"
-green "vps当前密码：$mima"
+green "vps当前root密码：$mima"
