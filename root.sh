f552dd838e7f908feaf97d07c537912db997f595
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-12-29 08:28:08 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,7 +3,7 @@
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
-
+[[ $EUID -ne 0 ]] && su='sudo' 
 lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
 chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
@@ -12,10 +12,10 @@ prl=`grep PermitRootLogin /etc/ssh/sshd_config`
 pa=`grep PasswordAuthentication /etc/ssh/sshd_config`
 if [[ -n $prl && -n $pa ]]; then
 read -p "自定义root密码:" mima
-echo root:$mima | chpasswd root
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-service sshd restart
+echo root:$mima | $su chpasswd root
+$su sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+$su sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+$su service sshd restart
 green "VPS当前用户名：root"
 green "vps当前root密码：$mima"
 else
